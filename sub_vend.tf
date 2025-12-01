7a5efacf518c0f339c8c9d415deb475a96424c4c
Matt Garner <m.garner@reply.com>
2025-12-01 16:06:46 +0000

    Update sub_vend.tf

--- sub_vend.tf
+++ sub_vend.tf
@@ -1,11 +1,11 @@
 resource "azurerm_role_assignment" "sub_vend_spn_not_in_use" {
   scope                = module.alz.management_group_resource_ids["${local.root_management_group_name}-not-in-use"]
   role_definition_id = module.alz.role_definition_resource_ids["${local.root_management_group_name}/Subscription-Mover"]
-  principal_id         = "7a4b6093-283d-4e85-a8d4-6a2573c923c7"
+  principal_id         = "cfaf1e73-c544-44f9-83b5-a7d22c8434e0"
 }
 
 resource "azurerm_role_assignment" "sub_vend_spn_landing_zones" {
   scope                = module.alz.management_group_resource_ids["${local.root_management_group_name}-landingzones"]
   role_definition_id = module.alz.role_definition_resource_ids["${local.root_management_group_name}/Subscription-Mover"]
-  principal_id         = "7a4b6093-283d-4e85-a8d4-6a2573c923c7"
+  principal_id         = "cfaf1e73-c544-44f9-83b5-a7d22c8434e0"
 }
