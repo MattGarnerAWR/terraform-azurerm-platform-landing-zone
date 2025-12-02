99286e1aa0f6e2784c3a5c2b34d71d95e6ae7ac6
Matt Garner <m.garner@reply.com>
2025-12-02 10:59:40 +0000

    dwa

--- sub_vend.tf
+++ sub_vend.tf
@@ -1,11 +1,11 @@
 resource "azurerm_role_assignment" "sub_vend_spn_not_in_use" {
-  scope                = module.alz.management_group_resource_ids["${local.root_management_group_name}-not-in-use"]
+  scope              = module.alz.management_group_resource_ids["${local.root_management_group_name}-not-in-use"]
   role_definition_id = module.alz.role_definition_resource_ids["${local.root_management_group_name}/Subscription-Mover"]
-  principal_id         = "cfaf1e73-c544-44f9-83b5-a7d22c8434e0"
+  principal_id       = "cfaf1e73-c544-44f9-83b5-a7d22c8434e0"
 }
 
 resource "azurerm_role_assignment" "sub_vend_spn_landing_zones" {
-  scope                = module.alz.management_group_resource_ids["${local.root_management_group_name}-landingzones"]
+  scope              = module.alz.management_group_resource_ids["${local.root_management_group_name}-landingzones"]
   role_definition_id = module.alz.role_definition_resource_ids["${local.root_management_group_name}/Subscription-Mover"]
-  principal_id         = "cfaf1e73-c544-44f9-83b5-a7d22c8434e0"
+  principal_id       = "cfaf1e73-c544-44f9-83b5-a7d22c8434e0"
 }
